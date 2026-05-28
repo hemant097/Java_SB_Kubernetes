c32efc7c71d7902fb608c5f3a298c7c5c96397b8
Hemant <hem789ant1997@outlook.com>
2026-05-28 19:10:09 +0530

    first commit

---
+++ src/main/java/com/example/project/kubelearn/KubeLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.kubelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeLearnApplication.class, args);
+    }
+
+}
